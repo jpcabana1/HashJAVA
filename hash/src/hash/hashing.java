51c3c1e9f1020c05d66583d3cb5e35a9a1872708
João Pedro <jpcabana1@gmail.com>
2019-05-08 18:35:47 -0300

    Add files via upload

---
+++ hash/src/hash/hashing.java
@@ -0,0 +1,19 @@
+package hash;
+
+public class hashing {
+	
+	
+	
+	public static void main(String[] args) {
+		ClasseHashing hash = new ClasseHashing();
+		
+		hash.setN(5);
+		
+		System.out.println(hash.dh(10));
+		
+
+	}
+
+}
+
+
